f73df03c095c150dd090c3078b95a3b7fccd9206
swetharagu <rswetha1997@gmail.com>
2017-04-19 13:54:58 +0530

    Add files via upload

---
+++ swetha.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int n,m;
+  printf("\n Enter the numbers");
+  scanf("%d",&n);
+  if(n>0)
+  {
+  printf("\n The given number is positive");
+  }
+  if(n<0)
+  {
+  printf("\n The given number is negative");
+  }
+  else
+  {
+  printf("\n zero");
+  }
+  return 0;
+  }
