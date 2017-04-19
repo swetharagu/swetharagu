6095a92188bbf73bf34b16db08dd08a8d828578a
swetharagu <rswetha1997@gmail.com>
2017-04-19 14:26:02 +0530

    Add files via upload

---
+++ swetha2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:");
+scanf("%d",num);
+if(num%2==0)
+printf("\n enter the number is even",num);
+else
+printf("\n enter the number is odd",num);
+return 0;
+}
