b860251f2218348c20555275cacc455a74bdcb6e
swetharagu <rswetha1997@gmail.com>
2017-06-16 14:35:27 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class number {
+	public static void main(String args[] ) {
+		Scanner in=new Scanner(System.in);
+
+		String s1;
+		String s2="";
+		System.out.println("enter string");
+		s1=in.nextLine();
+		int b=s1.length();
+		for(int i=b-1;i>=0;i--)
+		{
+			s2=s2+s1.charAt(i);
+		}
+		System.out.println(""+s2);
+	  
+	}
+}
+
