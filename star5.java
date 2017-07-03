9c1960cfe4e78370ecdad0c7af2ecf251f0a1516
swetharagu <rswetha1997@gmail.com>
2017-07-03 10:59:53 +0530

    Add files via upload

---
+++ star5.java
@@ -0,0 +1,28 @@
+package guvitest;
+
+import java.util.Scanner;
+
+public class star5 {
+
+	public static void main(String[] args)
+	{
+		  int rows, i, j;
+
+	        Scanner s = new Scanner(System.in);
+	        System.out.println("Enter number of rows in square pattern");
+	        rows = s.nextInt();
+
+	       
+	        for(i = 0; i < rows; i++){
+	           
+	            for(j = 0; j < rows; j++){
+	                System.out.print("*");
+	            }
+	            System.out.print("\n");
+	            s.close();
+	        }
+	    }
+	}
+	
+
+
