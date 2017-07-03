f0d8d42da942d68daf98cee7a6dafe1aa41a1243
swetharagu <rswetha1997@gmail.com>
2017-07-03 17:47:48 +0530

    Add files via upload

--- logic.java
+++ logic.java
@@ -1,20 +1,22 @@
 package dfs;
-
+import java.util.Arrays;
 import java.util.Scanner;
-
 public class logic {
-	public static void main(String[] args){
-	      Scanner sc=new Scanner(System.in);
-	      String s1;
-	      String S2 = new String();
-	  System.out.println("enter the string:");    
-	    String S1=sc.next();
-	      S2=new StringBuffer(S1).reverse().toString();
-	      
-	     
-	     
-	      System.out.println("String after reverse: "+S2);
-	   }
-	}
-	    
+	public static void main(String[] args)
+		 {
+		  System.out.println("ENTER THE NUMBER OF STUDENTS: ");
+		  Scanner s=new Scanner(System.in);
+		  int n=s.nextInt();
+		  double[] Arr=new double[n];
+		  System.out.println("ENTER THE HEIGHTS: ");
+		  for(int i=0;i<n;i++)
+		   {
+		     Arr[i]=s.nextDouble();
+		    }
+		  Arrays.sort(Arr);
+		  System.out.println("<<<4th TALLEST HEIGT IS:  "+Arr[n-4]+"  >>>");
+		  for(int i=1;i<=n;i++)
+		     System.out.println("\n"+i+"th TALLEST HEIGHT IS:"+Arr[n-i]);
+		 }
+		}
 	   
