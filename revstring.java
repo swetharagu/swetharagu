8856e5caf99387872af59f39580ebb87bc547484
swetharagu <rswetha1997@gmail.com>
2017-07-03 17:42:39 +0530

    Add files via upload

---
+++ revstring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class revstring {
+	public static void main(String[] args){
+	      Scanner sc=new Scanner(System.in);
+	      String s1;
+	      String S2 = new String();
+	  System.out.println("enter the string:");    
+	    String S1=sc.next();
+	      S2=new StringBuffer(S1).reverse().toString();
+	    System.out.println("String after reverse: "+S2);
+	   }
+	}
+	    
+
