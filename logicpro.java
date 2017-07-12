efdd94a2aee32122b827d3854ea4468bd40a70eb
swetharagu <rswetha1997@gmail.com>
2017-07-12 15:55:30 +0530

    Add files via upload

---
+++ logicpro.java
@@ -0,0 +1,38 @@
+package logic;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class logicpro {
+		public static void main(String[] args) {
+		     System.out.println("enter the input");
+		  Scanner sc=new Scanner(System.in);
+		  String s=sc.nextLine();
+			int m=s.length();
+			int j=0;
+			int g,n,k;
+				for(int i=0;i<m;i++){
+				g=s.charAt(i)-'0';
+				if(g==s.charAt(m-1)-'0')
+		{
+					n=s.charAt(0)-'0';
+			
+					k=pow(g,n);
+				}
+				else{
+			        n=s.charAt(i+1)-'0';
+			        k=pow(g,n);
+		                }j=j+k;}
+				System.out.println(j);
+			 }
+			 static  int pow(int g,int n){
+			    int total=1;
+			    for(int i=1;i<=n;i++){
+			        total*=g;
+		            }return total;
+			 }
+	}
+
+
+
