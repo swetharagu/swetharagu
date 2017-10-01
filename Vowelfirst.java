75d6141b031ab63ccc2538a5c2266eed7e3d2fff
swetharagu <rswetha1997@gmail.com>
2017-10-01 12:14:30 +0530

    Add files via upload

---
+++ Vowelfirst.java
@@ -0,0 +1,19 @@
+package Program;
+
+import java.util.Scanner;
+
+public class Vowelfirst {
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+    String str=s.next();
+    for(int i=0;i<str.length();i++) {
+    	char ch=str.charAt(i);
+    	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='O'||ch=='U') {
+    		System.out.print(ch);
+    	}
+   }
+   String r=str.replaceAll("[aeiou]","");
+   System.out.println(" ");
+System.out.print(r);
+}
+}
